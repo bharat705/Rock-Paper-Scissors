882e9336b6778c847bad952c409462dbf0ee80e5
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-09-03 07:22:18 +0545

    Add UI to the game
    User interface has been added but there is no feature to play multiple round just yet.

--- script.js
+++ script.js
@@ -1,59 +1,87 @@
 // Function to get computer choice using Math.random
 function getComputerChoice() {
-    let randomValue = Math.random()*100;
-    if (randomValue <= 33) {
-        return "rock";
-    } else if(randomValue <= 66) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+  let randomValue = Math.random() * 100;
+  if (randomValue <= 33) {
+    return "rock";
+  } else if (randomValue <= 66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
-// Function to prompt user for choice
-function getHumanChoice() {
-    let humanChoice = prompt("Please enter one: Rock/Paper/Scissor");
-    return humanChoice;
+
+const subtext = document.querySelector(".subtext");
+const resultBox = document.querySelector(".resultBox");
+const scoreBox = document.querySelector(".scoreBox");
+
+const humanResultBox = document.createElement("h3");
+const computerResultBox = document.createElement("h3");
+const resultRemarks = document.createElement("h3");
+
+const humanScoreBox = document.createElement("h2");
+const computerScoreBox = document.createElement("h2");
+
+// Function to update score in the screen
+function updateScore(human, computer) {
+  humanScoreBox.textContent = `Your Score: ${human}`;
+  computerScoreBox.textContent = `Computer Score: ${computer}`;
 }
 
 // Play the entire game
+function playGame(event, humanChoice) {
+  event.stopPropagation();
+  // Initialize players score variables
+  let humanScore = 0;
+  let computerScore = 0;
 
-function playGame() {
+  // Function to play a single round
+  function playRound(computerChoice) {
+    humanResultBox.textContent = `Your choice = ${humanChoice}`;
+    computerResultBox.textContent = `Computer choice = ${computerChoice}`;
+    resultBox.append(humanResultBox);
+    resultBox.append(computerResultBox);
 
-    // Initialize players score variables
-    let humanScore = 0;
-    let computerScore = 0;
-
-    // Function to play a single round 
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-        console.log(`Computer choice = ${computerChoice}`);
-        console.log(`Your choice = ${humanChoice}`);
-        if(humanChoice === computerChoice) {
-            return console.log("Draw!! Play Again.");
-        }
-        else if((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper") ) {
-            humanScore += 1;
-            console.log('Congratulations!! You Win. ');
-        } else {
-            computerScore += 1;
-            console.log(`You lose!! ${computerChoice} beats ${humanChoice}`);
-        }
-    }
-
-    for (let i = 1; i<=5; i++) {
-        // Get human and computer selection 
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    if(humanScore === computerScore) {
-        alert(`Tight game \n Your Score: ${humanScore}\n Computer Score: ${computerScore}`);
-    } else if (humanScore > computerScore) {
-        alert(`Congratulations! you are the champion \n Your Score: ${humanScore}\n Computer Score: ${computerScore}`);
+    if (humanChoice === computerChoice) {
+      resultRemarks.textContent = "That's a Draw!! Play Again.";
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      humanScore += 1;
+      updateScore(humanScore, computerScore);
+      resultRemarks.textContent = "Congratulations!! You Win. Keep it up! ";
     } else {
-        alert(`You lose!! Better luck next time \n Your Score: ${humanScore}\n Computer Score: ${computerScore}`);
+      computerScore += 1;
+      updateScore(humanScore, computerScore);
+      resultRemarks.textContent = `You lose!! ${computerChoice} beats ${humanChoice}`;
     }
+    resultBox.append(resultRemarks);
+    scoreBox.append(humanScoreBox);
+    scoreBox.append(computerScoreBox);
+  }
+
+  // Get computer selection
+  const computerSelection = getComputerChoice();
+  playRound(computerSelection);
+
+  if (humanScore === computerScore) {
+    alert(
+      `Tight game \n Your Score: ${humanScore}\n Computer Score: ${computerScore}`
+    );
+  } else if (humanScore > computerScore) {
+    alert(
+      `Congratulations! you are the champion \n Your Score: ${humanScore}\n Computer Score: ${computerScore}`
+    );
+  } else {
+    alert(
+      `You lose!! Better luck next time \n Your Score: ${humanScore}\n Computer Score: ${computerScore}`
+    );
+  }
 }
 
-playGame();
+const buttonBox = document.querySelector(".buttonBox");
+
+buttonBox.addEventListener("click", (e) => {
+  playGame(e, e.target.textContent.toLowerCase());
+});
